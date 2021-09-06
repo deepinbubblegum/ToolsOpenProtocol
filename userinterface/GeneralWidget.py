449b61bfed17803ec4c41bab5214a44a6b9308a9
deepinbubblegum <5911110222@mutacth.com>
2021-09-06 16:30:39 +0700

    add table

--- userinterface/GeneralWidget.py
+++ userinterface/GeneralWidget.py
@@ -13,8 +13,10 @@ class GeneralWidget(QWidget):
         VLayout = QVBoxLayout(self)
         HLayout1 = QHBoxLayout()
         HLayout2 = QHBoxLayout()
+        HLayout3 = QHBoxLayout()
         VLayout.addLayout(HLayout1)
         VLayout.addLayout(HLayout2)
+        VLayout.addLayout(HLayout3)
         VLayout.addStretch()
 
         label_IPAddress = QLabel("IP Address :")
@@ -44,3 +46,23 @@ class GeneralWidget(QWidget):
         HLayout2.addWidget(label_Link_ID)
         HLayout2.addWidget(combo_Link_ID, 1)
 
+        table = QTableWidget(self)
+        table.setColumnCount(4)
+        table.setRowCount(50)
+        # Set the table headers
+        table.setHorizontalHeaderLabels(["STEP ID", "TRAY ID", "SOCKET ID", "COMMAND"])
+        table.setAlternatingRowColors(True)
+        table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+
+        # table.horizontalHeader().setSectionResizeMode(0, QHeaderView.ResizeToContents)
+        # table.horizontalHeader().setSectionResizeMode(1, QHeaderView.ResizeToContents)
+        # table.horizontalHeader().setSectionResizeMode(3, QHeaderView.ResizeToContents)
+
+        # Set the table values
+        for i in range(50):
+            for j in range(4) :
+                table.setItem(i, j, QTableWidgetItem("Row-" + str(i+1) + " , Col-" + str(j+1)))
+        # Resize of the rows and columns based on the content
+        table.resizeColumnsToContents()
+        table.resizeRowsToContents()
+        HLayout3.addWidget(table , 1)
