3c2166e38a577f44eb6f0039343b199285f85ef4
deepinbubblegum <5911110222@mutacth.com>
2021-09-09 11:48:31 +0700

    update

--- lib/modbus.py
+++ lib/modbus.py
@@ -20,92 +20,77 @@ class TrayModbus():
         self.instrument.mode = minimalmodbus.MODE_RTU   # rtu or ascii mode
         self.instrument.clear_buffers_before_each_transaction = True
 
-        self.socket_pickup = 1
+        self.socket_pickup = None
         self.bing_picked = False
 
         self.socket_tray_check_obj = False
-        self.socket_tray = {
-            1: 1, 2: 1, 3: 1, 4: 1,
-            5: 1, 6: 1, 7: 1, 8: 1,
-        }
+        self.socket_tray = [
+            0,0,0,0,
+            0,0,0,0
+        ]
         
-        self.color_rgb = [0x0F0, 0x000, 0x0F0]
+        self.leds_socket_tray = [
+            0x000,0x000,0x000,0x000,
+            0x000,0x000,0x000,0x000
+        ]
         
-        self.thread_tray_socket = threading.Thread(target=self.have_obj_socket)
+        self.color_rgb = [0xF00, 0x0F0, 0x00F, 0x000]
+        self.color_g_bing = [0x000, 0x0F0]
+        
+        self.thread_tray_socket = threading.Thread(target=self.Thread_Tray_socket)
         self.thread_tray_socket.daemon = True
         self.thread_tray_socket.start()
+        
+        self.thread_event_socket = threading.Thread(target=self.event_socket_tray)
+        self.thread_event_socket.daemon = True
+        self.thread_event_socket.start()
 
-    def bing_led(self, addr):
-        for color in self.color_rgb:
-            if self.bing_picked:
-                break
-            time.sleep(0.05)
-            self.write_register(
-                registeraddress=addr, 
-                value=color,
-                mode=0
-            )
-
-    # Check object all socket in hole
-    def have_obj_socket(self):
+    def Thread_Tray_socket(self):
         while True:
-            if self.socket_tray_check_obj is False:
-                for index in range(len(self.socket_tray)):
-                    res_obj_socket_tray = self.read_register(
-                        registeraddress = index,
-                        numberOfDecimals = 0,
-                        functioncode = 4,
-                        signed = False
-                    )
-                    self.socket_tray[index + 1] = res_obj_socket_tray
-                    if bool(res_obj_socket_tray) is False and (index + 1) != self.socket_pickup:
-                        self.write_register(
-                            registeraddress=index, 
-                            value=0x00F,
-                            mode=0
-                        )
-                    elif bool(res_obj_socket_tray) is False and (index + 1) == self.socket_pickup:
-                        # pick scoket
-                        try:
-                            self.bing_led(index)
-                        except AttributeError:
-                            pass
-                    elif bool(res_obj_socket_tray) and (index + 1) == self.socket_pickup:
-                        self.bing_picked = True
-                        self.write_register(
-                            registeraddress=index, 
-                            value=0x0F0,
-                            mode=0
-                        )
-                    elif bool(res_obj_socket_tray) and (index + 1) != self.socket_pickup:
-                        self.bing_picked = True
-                        self.write_register(
-                            registeraddress=index, 
-                            value=0xF00,
-                            mode=0
-                        )
-                self.socket_tray_check_obj = False
-                self.bing_picked = True
+            for idx_add in range(len(self.socket_tray)):
+                self.socket_tray[idx_add] = self.read_register(
+                    registeraddress = idx_add,
+                    numberOfDecimals = 0,
+                    functioncode = 4,
+                    signed = False
+                )
+                self.write_register(
+                    registeraddress=idx_add, 
+                    value=self.leds_socket_tray[idx_add],
+                    mode=0
+                )
+                
+    def event_socket_tray(self):
+        idx_bing_led = 0
+        while True:
+            for idx in range(len(self.socket_tray)):
+                if bool(self.socket_tray[idx]) is False and (idx + 1) != self.socket_pickup:
+                    self.leds_socket_tray[idx] = self.color_rgb[2]
+                elif bool(self.socket_tray[idx]) is False and (idx + 1) == self.socket_pickup:
+                    self.leds_socket_tray[idx] = self.color_rgb[1]
+                elif bool(self.socket_tray[idx]) and (idx + 1) == self.socket_pickup:
+                    self.leds_socket_tray[idx] = self.color_g_bing[idx_bing_led]
+                elif bool(self.socket_tray[idx]) and (idx + 1) != self.socket_pickup:
+                    self.leds_socket_tray[idx] = self.color_rgb[0]  
+            if idx_bing_led >= 1:
+                idx_bing_led = 0
+            else:
+                idx_bing_led += 1
+            time.sleep(0.25)
 
     def set_socket_pickup(self, value):
         self.socket_pickup = value
         
     def write_register(self, registeraddress, value, mode=0):
-        try:
-            self.instrument.write_register(registeraddress, value, mode)
-        except AttributeError:
-            pass
+        self.instrument.write_register(registeraddress, value, mode)
 
     def read_register(self, registeraddress, mode=0):
-        try:
-            res_value = self.instrument.read_register(
-                registeraddress, 
-                mode
-            )  # Registernumber, number of decimals
-            return hex(res_value)
-        except AttributeError:
-            pass
-
+        res_value = self.instrument.read_register(
+            registeraddress, 
+            mode
+        )  # Registernumber, number of decimals
+        return hex(res_value)
+    
     def read_register(self, registeraddress, numberOfDecimals = 0, functioncode = 4, signed = False):
         registervalue = self.instrument.read_register(
             registeraddress,
